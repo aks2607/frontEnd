bca27bfe07efb0157704ea1f1c50678e2221ee1e
aks2607 <avinash.singh2607@gmail.com>
2025-07-19 18:22:46 +0530

    Create onceFnCall.js

---
+++ onceFnCall.js
@@ -0,0 +1,13 @@
+export default function once(func) {
+  let ranOnce = false;
+  let value;
+
+  return function (...args) {
+    if (!ranOnce) {
+      value = func.apply(this, args);
+      ranOnce = true;
+    }
+
+    return value;
+  };
+}
